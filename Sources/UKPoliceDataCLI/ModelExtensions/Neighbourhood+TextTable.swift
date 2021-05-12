b18667fec519e0fa6590c22cdd05507a069a35f9
Adam Young <me@adam-young.co.uk>
2021-05-12 23:38:27 +0100

    Fixed linting errors

--- Sources/UKPoliceDataCLI/ModelExtensions/Neighbourhood+TextTable.swift
+++ Sources/UKPoliceDataCLI/ModelExtensions/Neighbourhood+TextTable.swift
@@ -15,70 +15,37 @@ extension Neighbourhood {
         table.addRow(values: ["Name", name])
         table.addRow(values: ["Population", population])
         table.addRow(values: ["Website", policeForceWebsite])
-
-        if let email = contactDetails.email {
-            table.addRow(values: ["Email", email])
-        }
-
-        if let telephone = contactDetails.telephone {
-            table.addRow(values: ["Telephone", telephone])
-        }
-
-        if let mobile = contactDetails.mobile {
-            table.addRow(values: ["Mobile", mobile])
-        }
-
-        if let fax = contactDetails.fax {
-            table.addRow(values: ["Fax", fax])
-        }
-
-        if let web = contactDetails.web {
-            table.addRow(values: ["Web", web])
-        }
-
-        if let address = contactDetails.address {
-            table.addRow(values: ["Address", address.replacingOccurrences(of: "\n", with: ", ")])
-        }
-
-        if let facebook = contactDetails.facebook {
-            table.addRow(values: ["Facebook", facebook])
-        }
-
-        if let twitter = contactDetails.twitter {
-            table.addRow(values: ["Twitter", twitter])
-        }
-
-        if let youtube = contactDetails.youtube {
-            table.addRow(values: ["YouTube", youtube])
-        }
-
-        if let myspace = contactDetails.myspace {
-            table.addRow(values: ["MySpace", myspace])
-        }
-
-        if let bebo = contactDetails.bebo {
-            table.addRow(values: ["Bebo", bebo])
-        }
-
-        if let flickr = contactDetails.flickr {
-            table.addRow(values: ["Flickr", flickr])
-        }
-
-        if let forum = contactDetails.forum {
-            table.addRow(values: ["Forum", forum])
-        }
-
-        if let blog = contactDetails.blog {
-            table.addRow(values: ["Blog", blog])
-        }
-
-        if let rss = contactDetails.rss {
-            table.addRow(values: ["RSS", rss])
-        }
-
+        table.optionallyAddRow(field: "Email", value: contactDetails.email)
+        table.optionallyAddRow(field: "Telephone", value: contactDetails.telephone)
+        table.optionallyAddRow(field: "Mobile", value: contactDetails.mobile)
+        table.optionallyAddRow(field: "Fax", value: contactDetails.fax)
+        table.optionallyAddRow(field: "Web", value: contactDetails.web)
+        table.optionallyAddRow(field: "Address",
+                               value: contactDetails.address?.replacingOccurrences(of: "\n", with: ", "))
+        table.optionallyAddRow(field: "Facebook", value: contactDetails.facebook)
+        table.optionallyAddRow(field: "Twitter", value: contactDetails.twitter)
+        table.optionallyAddRow(field: "YouTube", value: contactDetails.youtube)
+        table.optionallyAddRow(field: "MySpace", value: contactDetails.myspace)
+        table.optionallyAddRow(field: "Bebo", value: contactDetails.bebo)
+        table.optionallyAddRow(field: "Flickr", value: contactDetails.flickr)
+        table.optionallyAddRow(field: "Forum", value: contactDetails.forum)
+        table.optionallyAddRow(field: "Blog", value: contactDetails.blog)
+        table.optionallyAddRow(field: "RSS", value: contactDetails.rss)
         table.addRow(values: ["Coordinates", centre])
 
         return table.render()
+    }
+
+}
+
+private extension TextTable {
+
+    mutating func optionallyAddRow(field: String, value: CustomStringConvertible?) {
+        guard let value = value else {
+            return
+        }
+
+        addRow(values: [field, value])
     }
 
 }
